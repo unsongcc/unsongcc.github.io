531e9c1945f2a6441225a33a5eaf0043bbcb47ff
unsongcc <unsongcc@hotmail.com>
2024-10-25 02:50:54 +0800

    Update statistics.js

--- static/statistics.js
+++ static/statistics.js
@@ -1,1 +1,1 @@
-//
+var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?d522d12821ff9d3f58b263dd4f984bc7";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();
