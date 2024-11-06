93629186327d41bab15ae37726750fe040f36842
unsongcc <unsongcc@hotmail.com>
2024-11-06 11:35:40 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
-var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?25857709e83d10088dfbcaabcb5e1154";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?25857709e83d10088dfbcaabcb5e1154";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 var userAgent = navigator.userAgent;
 var spiderList = ["Baiduspider", "360Spider", "YisouSpider", "YandexBot", "Sogou inst spider", "Sogou web spider", "msnbot", "Bingbot", "Sogou spider", "GoogleBot", "Bing"];
 
