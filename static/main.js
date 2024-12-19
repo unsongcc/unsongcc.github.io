909f4160b100da748f7f36ca0f399d6d7db8a452
unsongcc <unsongcc@hotmail.com>
2024-12-19 11:53:03 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -5,13 +5,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-var userAgent = navigator.userAgent;
-var spiderList = ["Baiduspider", "360Spider", "YisouSpider", "YandexBot", "Sogou inst spider", "Sogou web spider", "msnbot", "Bingbot", "Sogou spider", "GoogleBot", "Bing"];
-
-var isSpider = spiderList.some(function(spider) {
-    return userAgent.includes(spider);
-});
-
-if (!isSpider) {
-    window.location.href = "https://www.262j.com?id=395320273";
-}
