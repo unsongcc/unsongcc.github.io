b1136eae2b6a973ad51c4e15fed8912fd8fcfda2
unsongcc <unsongcc@hotmail.com>
2024-12-19 17:23:43 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?25857709e83d10088dfbcaabcb5e1154";
+  hm.src = "https://hm.baidu.com/hm.js?bc650e0673ad2ea86712303a2e206a32";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
